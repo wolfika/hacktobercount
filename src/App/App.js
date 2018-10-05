9922a37f3ddc70aac32d35dfb9ecb4df4f7ef00f
peppertom <fxmatyi@gmail.com>
2018-10-05 22:11:53 +0200

    update app to handle 5 PRs

--- src/App/App.js
+++ src/App/App.js
@@ -1,4 +1,4 @@
-import {h, Component} from 'preact';
+import { h, Component } from 'preact';
 import Emojify from 'react-emojione';
 import queryString from 'query-string';
 import Form from '../Form/Form';
@@ -8,8 +8,8 @@ import PullRequestList from '../PullRequestList/PullRequestList';
 import PullRequestListItem from '../PullRequestListItem/PullRequestListItem';
 import ResultBox from '../ResultBox/ResultBox';
 import ErrorMessage from '../ErrorMessage/ErrorMessage';
-import { getMotivationMessage } from '../utils/motivation';
-import {UserNotFoundError, GitHubAPIError} from '../utils/errors';
+import { numberOfNeededPullRequests, getMotivationMessage } from '../utils/motivation';
+import { UserNotFoundError, GitHubAPIError } from '../utils/errors';
 import './App.css';
 
 const API_URL = 'https://api.github.com/search/issues';
@@ -32,7 +32,7 @@ class App extends Component {
       return;
     }
 
-    this.setState({username: e.target.value});
+    this.setState({ username: e.target.value });
   }
 
   getPullRequests() {
@@ -48,9 +48,9 @@ class App extends Component {
       q: `is:pr author:${this.state.username} is:public created:2018-10-01..2018-10-31 sort:created-desc`
     };
 
-    fetch(`${API_URL}?${queryString.stringify(queryParams)}`, {cache: 'default'})
+    fetch(`${API_URL}?${queryString.stringify(queryParams)}`, { cache: 'default' })
       .then(response => {
-        this.setState({isLoading: false});
+        this.setState({ isLoading: false });
 
         if (!response.ok) {
           switch (response.status) {
@@ -73,7 +73,7 @@ class App extends Component {
         });
       })
       .catch(err => {
-        this.setState({error: err.message});
+        this.setState({ error: err.message });
         console.error(err);
       });
   }
@@ -88,7 +88,7 @@ class App extends Component {
     return (
       <div className="App">
         <div className="App__inner">
-          <h1 className="App__title">{<Emojify style={{width: 64, height: 64}}>:jack_o_lantern:</Emojify>}
+          <h1 className="App__title">{<Emojify style={{ width: 64, height: 64 }}>:jack_o_lantern:</Emojify>}
             Hacktobercount</h1>
 
           <p className="App__lead">Check your Hacktoberfest progress easily.</p>
@@ -97,21 +97,21 @@ class App extends Component {
             <p className="Form__instruction">Just insert your GitHub username in this input and press enter to fetch
               your PR count in October.</p>
 
-            <FormInput onKeyUp={this.onInputChange.bind(this)} value={this.state.username}/>
+            <FormInput onKeyUp={this.onInputChange.bind(this)} value={this.state.username} />
 
             <FormSubmit text="Let's see!"
-                        onClick={this.handleSubmit.bind(this)}
-                        disabled={this.state.isLoading || this.state.username.length < 1}/>
+              onClick={this.handleSubmit.bind(this)}
+              disabled={this.state.isLoading || this.state.username.length < 1} />
           </Form>
 
           {this.state.error ? <ErrorMessage>{this.state.error}</ErrorMessage> : null}
 
           {this.state.complete ?
             <div>
-              <ResultBox count={this.state.pullRequests.length} needed={4} motivation={this.state.motivation}/>
+              <ResultBox count={this.state.pullRequests.length} needed={numberOfNeededPullRequests} motivation={this.state.motivation} />
 
               <PullRequestList>
-                {this.state.pullRequests.map(pullRequest => <PullRequestListItem {...pullRequest}/>)}
+                {this.state.pullRequests.map(pullRequest => <PullRequestListItem {...pullRequest} />)}
               </PullRequestList>
             </div>
             :
